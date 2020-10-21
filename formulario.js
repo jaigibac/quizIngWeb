ddee69a141e3ea20425d07b8e507412c54af93e6
jaigibac <71862609+jaigibac@users.noreply.github.com>
2020-10-21 17:01:10 -0300

    Add files via upload

---
+++ formulario.js
@@ -0,0 +1,8 @@
+function mostrarOcultar(id1, id2) {
+    document.getElementById(id1).style.display = 'block';
+    document.getElementById(id2).style.display = 'none';
+}
+;
+function confimacionEnvioDatos(id) {
+    document.getElementById(id).innerHTML = "<p id='datos'>datos enviados<p>";
+}
